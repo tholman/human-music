5803a503a29145773e8e986cddda193085a09c00
Tim <timothy.w.holman@gmail.com>
2016-11-24 12:11:08 -0500

    website done

--- js/human-music.js
+++ js/human-music.js
@@ -1,10 +1,17 @@
-/*!
- * human-music
+/**
+ * Driving home with Human Music
  *
  * MIT licensed
- * Copyright (C) 2013 Tim Holman, http://tholman.com
+ * Copyright (C) 2016 Tim Holman, http://tholman.com
  */
 
-/*********************************************
- *
- *********************************************/
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+// Load Facebook
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
